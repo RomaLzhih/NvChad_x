e22a4c456aeb707667f5a4bda4cd1f9bf03cbdd0
RomaLzhih <791191656@qq.com>
2023-12-09 13:42:32 -0800

    update neovide

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -12,3 +12,17 @@ vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decr
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+
+if vim.g.neovide then
+  -- Put anything you want to happen only in Neovide here
+  vim.o.guifont = "DankMono Nerd Font:h12"
+  vim.g.neovide_transparency = 0.9
+  vim.g.neovide_cursor_animation_length = 0.1
+  vim.g.neovide_cursor_trail_size = 0.5
+  vim.g.neovide_cursor_vfx_mode = "torpedo"
+  vim.g.neovide_padding_top = 0
+  vim.g.neovide_padding_bottom = 0
+  vim.g.neovide_padding_right = 0
+  vim.g.neovide_padding_left = 0
+  vim.g.neovide_remember_window_size = true
+end
