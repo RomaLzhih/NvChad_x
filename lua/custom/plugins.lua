218d7ae6bf7fa3cf21425f38f503ca33913c549f
ziyang <791191656@qq.com>
2023-12-10 20:15:13 -0800

    update hop keyboard

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -304,16 +304,16 @@ local plugins = {
       local hop = require "hop"
       local directions = require("hop.hint").HintDirection
       vim.keymap.set("", "f", function()
-        hop.hint_char2 { direction = directions.AFTER_CURSOR }
+        hop.hint_char1 { direction = directions.AFTER_CURSOR }
       end, { remap = true })
       vim.keymap.set("", "F", function()
         hop.hint_char2 { direction = directions.BEFORE_CURSOR }
       end, { remap = true })
       vim.keymap.set("", "t", function()
-        hop.hint_char2 { direction = directions.AFTER_CURSOR, hint_offset = -1 }
+        hop.hint_char1 { direction = directions.AFTER_CURSOR }
       end, { remap = true })
       vim.keymap.set("", "T", function()
-        hop.hint_char2 { direction = directions.BEFORE_CURSOR, hint_offset = 1 }
+        hop.hint_char2 { direction = directions.BEFORE_CURSOR }
       end, { remap = true })
     end,
     lazy = false,
