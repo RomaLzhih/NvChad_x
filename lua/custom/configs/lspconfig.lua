7c68556715c5eba171e0f79ffc62e68a8e47c177
ziyang <791191656@qq.com>
2023-12-12 17:09:12 -0800

    add texlab

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "bashls", "pyright", "clangd", "r_language_server" }
+local servers = { "bashls", "pyright", "clangd", "r_language_server", "texlab" }
 
 -- vim.notify = require("noice").notify
 -- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
