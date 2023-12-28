cc30ebeb3b8f3b63ee48229e0a5bf7c4a7461f39
ziyang <791191656@qq.com>
2023-12-27 17:49:49 -0800

    update pylint

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -262,7 +262,7 @@ local plugins = {
       local lint = require "lint"
 
       lint.linters_by_ft = {
-        python = { "pylint" },
+        -- python = { "pylint" },
         cpp = { "clangtidy", "cppcheck" },
         bash = { "shellcheck" },
       }
