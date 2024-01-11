415a7de9c1eef6fcb2b860053b123d5032e32ac4
ziyang <791191656@qq.com>
2024-01-10 18:29:48 -0800

    update themes

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -8,6 +8,25 @@ local plugins = {
   },
 
   {
+    "ribru17/bamboo.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("bamboo").setup {
+        -- optional configuration here
+      }
+    end,
+  },
+
+  {
+    "EdenEast/nightfox.nvim",
+    lazy = false,
+    config = function()
+      require("nightfox").setup()
+    end,
+  },
+
+  {
     "AlexvZyl/nordic.nvim",
     lazy = false,
     priority = 1000,
